ecbb7396e7c1df8d3c8d6f312513d498cdf405e6
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-03-26 18:33:50 -0500

    feat: implement `GetServiceTypes` in `Neo4jReportRepository` for fetching service type counts

---
+++ neo4jrepositories/reportrepository/get_service_types.go
@@ -0,0 +1,49 @@
+package reportrepository
+
+import (
+	"context"
+	"errors"
+	"service-atlas/repositories"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func (r Neo4jReportRepository) GetServiceTypes(ctx context.Context) ([]repositories.ServiceType, error) {
+	typeReport := make([]repositories.ServiceType, 2)
+	_, err := r.manager.ExecuteRead(ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		typeCountQuery := `
+			MATCH (s:Service)
+			RETURN s.type AS type, count(*) AS count
+			ORDER BY count DESC
+			`
+		result, err := tx.Run(ctx, typeCountQuery, nil)
+
+		if err != nil {
+			return nil, err
+		}
+		records, err := result.Collect(ctx)
+		if err != nil {
+			return nil, err
+		}
+		for _, record := range records {
+			t, ok := record.Get("type")
+			if !ok {
+				return nil, errors.New("type not found")
+			}
+			serviceType := repositories.ServiceType{}
+			serviceType.Type = t.(string)
+			count, ok := record.Get("count")
+			if !ok {
+				return nil, errors.New("count not found")
+			}
+			serviceType.Count = count.(int64)
+			typeReport = append(typeReport, serviceType)
+		}
+
+		return nil, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return typeReport, nil
+}
