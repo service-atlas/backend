f1439e199ddb67111426fbc4e3f78a557478bc70
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-04-09 20:33:32 -0500

    feat: rename `DependencyType` to `InteractionType` in `Dependency` struct

--- repositories/dependency.go
+++ repositories/dependency.go
@@ -3,11 +3,11 @@ package repositories
 import "errors"
 
 type Dependency struct {
-	Id             string `json:"id"`
-	Version        string `json:"version,omitempty"`
-	Name           string `json:"name,omitempty"`
-	ServiceType    string `json:"type,omitempty"`
-	DependencyType string `json:"dependency_type,omitempty"`
+	Id              string `json:"id"`
+	Version         string `json:"version,omitempty"`
+	Name            string `json:"name,omitempty"`
+	ServiceType     string `json:"type,omitempty"`
+	InteractionType string `json:"interaction_type,omitempty"`
 }
 
 func (d *Dependency) Validate() error {
